f43196df3a20a172851a17627334fec6f82c9f7d
gregvoganesyan <gregvoganesyan@gmail.com>
2025-10-08 14:34:32 -0500

    Update SnakeGame.py

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,1 +1,17 @@
+#Gregory Oganesyan
+#Jonathan Reyes
+#Matthew Vasquez
+
+import random
+
+minutesRemaining = 5
+randomNum = 0
+
+print(f"Congrats, you are now a snake, and you are trapped inside a house!")
+print(f"You hear a hunter approaching and you have "+minutesRemaining+"until the hunter catches you!")
+print(f"You have to get to the LIVING ROOM before he finds you!")
+print(f"However, you need energy to get to LIVING ROOM. You can choose to eat a BIRD, LIZARD, or RAT. Only one option will give you enough energy to make it...")
+# make a while loop that does not break out until the user guesses the right random number
+# if minutes remaining <1, then the game is over
+
 
